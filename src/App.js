8f3c53a52e4550442a47b6fc1ef6a303184fdaf6
Newsroom <dhritisikha@avantari.org>
2022-08-25 02:01:37 +0530

    header add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Suspense, useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Header from "./components/header";
+// import Header from "./components/header";
 
 function App() {
   const [allNewsData, setAllNewsData] = useState([]);
@@ -22,7 +22,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
 
         <div>
           <Suspense
