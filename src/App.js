490b7434120165c280faa59ebb2cda62eb47b0cf
Newsroom <dhritisikha@avantari.org>
2022-08-25 01:59:29 +0530

    header add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Header from "./components/header";
 
 function App() {
+  const [allNewsData, setAllNewsData] = useState([]);
+  const url = "https://newsroom-backend.herokuapp.com/newsPortal";
+  const WAIT_TIME = 500;
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      axios.get(url).then((response) => {
+        response.data.newsPortal.reverse();
+        setAllNewsData(response.data.newsPortal);
+      });
+    }, WAIT_TIME);
+    return () => clearInterval(id);
+  }, [allNewsData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <div>
+          <Suspense
+            fallback={
+              <div>
+                <div>Loading...</div>
+              </div>
+            }
+          >
+            {/* <Routes>
+            <Route
+              path="/"
+              element={<HomeComponent allNewsData={allNewsData} />}
+            />
+            <Route path="/news/:id" element={<SelectedNews />} />
+            <Route
+              path="/news/top-story"
+              element={<TopStoryComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/politics"
+              element={<PoliticsComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/features"
+              element={<FeaturesComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/courtRoom"
+              element={<CourtRoomComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/unexploredTN"
+              element={<UnTNComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/environment"
+              element={<EnvironmentComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/reports"
+              element={<ReportsComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/columns"
+              element={<ColumnsComponent allNewsData={allNewsData} />}
+            />
+            <Route
+              path="/news/videos"
+              element={<VideosComponent allNewsData={allNewsData} />}
+            />
+          </Routes> */}
+          </Suspense>
+        </div>
+        <div>{/* <NewsRoomComponent /> */}</div>
+        <div>{/* <Footer /> */}</div>
+      </BrowserRouter>
+    </>
   );
 }
 
